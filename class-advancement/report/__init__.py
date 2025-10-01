c76af2fbff7eb57af908820375e5d0e97995d3b2
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2025-09-30 21:46:09 -0400

    Adding tool to report advancement

--- class-advancement/report/__init__.py
+++ class-advancement/report/__init__.py
@@ -1,4 +1,4 @@
 from .curl_backend import register_done_curl, submit_done
 from .ui import show_done_button, done_html
 
-__all__ = ["register_done_curl", "submit_done", "show_done_button", "done_html"]
+__all__ = ["register_done_curl", "show_done_button", "done_html"]
